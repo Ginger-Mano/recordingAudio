48b2053d1a6186fa5c6d148130df6dcb31f6f065
Manon Sainton <manonsainton.dev@gmail.com>
2021-05-24 18:23:40 -0400

    add buttons

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 let webpage = document.querySelector(".main")
 
-let text = document.createElement('h2')
-    text.innerHTML = "new file"
+let audioBlock = (() => {
+    let headingDiv = document.createElement('div')
+        headingDiv.className = "header"
 
-    webpage.append(text)
+    let pageTitle = document.createElement('h2')
+        pageTitle.className = "pageTitle"
+        pageTitle.innerText = "Recording Audio"
+
+        let recordButton = document.createElement("button")
+        recordButton.className = "button"
+        recordButton.innerText = "record"
+
+        let playButton = document.createElement("button")
+        playButton.className = "button"
+        playButton.innerText = "play"
+
+        let stopButton = document.createElement("button")
+        stopButton.className = "button"
+        stopButton.innerText = "stop"
+
+        let deleteButton = document.createElement("button")
+        deleteButton.className = "button"
+        deleteButton.innerText = "delete"
+
+        let recordingBlock = document.createElement("div")
+        recordingBlock.className = "recBlock"
+
+        let recordingClip = document.createElement("input")
+        recordingClip.className = "recording"
+
+        headingDiv.append(pageTitle)
+        recordingBlock.append(recordingClip, recordButton, playButton, stopButton, deleteButton)
+        webpage.append(headingDiv, recordingBlock)
+
+})() 
